41190569411ede1b9a87035d797bd285aea35203
Igor Vieira <igor.p.r.vieira@gmail.com>
2017-06-01 11:29:44 -0300

    setup for Heroku button

--- app.js
+++
@@ -1,6 +0,0 @@
-{
-  "name": "Node.js Base API",
-  "description": "Base API for build another apps",
-  "repository": "https://github.com/IgorVieira/base-api",
-  "keywords": ["node", "express", "static"]
-} 
