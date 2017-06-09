3435b62b41fe89f6e353674220cd356cdd26638d
Igor Vieira <igor.p.r.vieira@gmail.com>
2017-06-09 10:57:50 -0300

    setup for access API and complete functions with a CRUD

--- server/index.js
+++ server/index.js
@@ -9,6 +9,13 @@ const app = express();
 
 require('./database')(config.mongodb.uri);
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+
 app.use(express.static('./client'));
 app.use(bodyParser.urlencoded(config.bodyParser));
 app.use(bodyParser.json());
