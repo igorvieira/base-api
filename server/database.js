53d835b611a00b353f18867df49d4b59699a6eb7
Igor Vieira <igor.p.r.vieira@gmail.com>
2017-08-02 16:13:14 -0300

    Fixed problem about open connection on mongodb

--- server/database.js
+++ server/database.js
@@ -3,24 +3,27 @@ import mongoose from 'mongoose';
 mongoose.Promise = require('bluebird');
 
 module.exports = (uri) => {
-  mongoose.connect(uri);
-  mongoose.connection.on('connected', () => {
-    console.log(`Mongoose! Connected in:${uri}`);
-  });
+	mongoose
+	.connect(uri, {
+		useMongoClient: true,
+	});
+	mongoose.connection.on('connected', () => {
+		console.log(`Mongoose! Connected in:${uri}`);
+	});
 
 
-  mongoose.connection.on('disconnect', () => {
-    console.log(`Mongoose! Desconnected:${uri}`);
-  });
+	mongoose.connection.on('disconnect', () => {
+		console.log(`Mongoose! Desconnected:${uri}`);
+	});
 
-  mongoose.connection.on('error', (erro) => {
-    console.log(`Mongoose!Erro connection${erro}`);
-  });
+	mongoose.connection.on('error', (erro) => {
+		console.log(`Mongoose!Erro connection${erro}`);
+	});
 
-  process.on('SIGINT', () => {
-    mongoose.connection.close(() => {
-      console.log('Good bye =]');
-      process.exit(0);
-    });
-  });
+	process.on('SIGINT', () => {
+		mongoose.connection.close(() => {
+			console.log('Good bye =]');
+			process.exit(0);
+		});
+	});
 };
